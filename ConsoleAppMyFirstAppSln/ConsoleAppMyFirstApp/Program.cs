88d7c827fcd7ab3f74b2275efbc04affcbd60691
nijatabdull <nijattttsa@gmail.com>
2024-11-05 19:39:06 +0400

    Initial commit

---
+++ ConsoleAppMyFirstAppSln/ConsoleAppMyFirstApp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleAppMyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Surname: ");
+            string surname = Console.ReadLine();
+
+            Console.Write("Age: ");
+            string inputAge = Console.ReadLine();
+
+            byte age = Convert.ToByte(inputAge);
+
+            //byte age = 25;
+            //double salary = 500.50;
+            //char gender = 'm';
+            //bool hasEmail = false;
+            //DateTime birthDate = new DateTime(1999,10,29);
+
+            Console.WriteLine("New student:");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Surname: " + surname);
+            Console.WriteLine("Age: " + age);
+        }
+    }
+}
---
+++ ConsoleAppMyFirstAppSln/ConsoleAppRectangeArea/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleAppRectangeArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+             int b = ++a;
+
+            Console.ReadKey();
+        }
+    }
+}
